f441e98d74bf8a08a1f477abcfbc5f5348c3760d
Huy Le <huyle@arizona.edu>
2024-07-24 11:06:14 -0700

    bug fixes

--- start.py
+++ start.py
@@ -1,15 +1,20 @@
 import json
+from time import sleep
 from scrapy.crawler import CrawlerProcess
 from phongvu_scraper.spiders.product import ProductSpider
 
-with open('./login.json') as f:
-    login = json.load(f)
-    username = login['username']
-    password = login['password']
-    cluster = login['cluster@']
-    if len(username) == 0 or len(password) == 0 or len(cluster) == 0:
-        print("Invalid login credentials")
-        exit()
+while True:
+    with open('./login.json') as f:
+        login = json.load(f)
+        username = login['username']
+        password = login['password']
+        cluster = login['cluster@']
+        if len(username) == 0 or len(password) == 0 or len(cluster) == 0:
+            print('Please fill in the login.json file')
+            sleep(1)
+            continue
+        else:
+            break
 
 process = CrawlerProcess()
 process.crawl(ProductSpider)
